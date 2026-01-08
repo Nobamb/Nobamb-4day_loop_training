cc5ab3c56cc4ae60d25a3573c1373b4ec4554469
nobamb <jsh12345644@gmail.com>
2026-01-09 00:12:31 +0900

    training22 완료

--- training22.py
+++ training22.py
@@ -49,3 +49,52 @@
 # 공미남 27
 
 # ```
+
+# 입력 받음
+name = input("이름")
+
+
+# Person 클래스 생성
+# name, age 받음
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+# PersonManage 클래스
+class PersonManage:
+    def __init__(self):
+        # 배열
+        self.manage = []
+    # 배열에 값 추가
+    def add(self, name, age):
+        # person 생성
+        person  = Person(name, age)
+        # 배열에 값 추가
+        self.manage.append(person)
+        
+    # 이름으로 값찾기
+    def read_by_name(self, name):
+        # manage 순회
+        for member in self.manage:
+            # 이름이 같으면
+            if member.name == name:
+                # 값 리턴
+                return member
+            
+        # 없으면 none
+        return None
+    
+# 생성자 생성
+manager = PersonManage()
+
+# 추가
+manager.add("공욱재", 26)
+manager.add("공미남", 27)
+
+# 이름 찾기
+result = manager.read_by_name(name)
+
+# 값이 존해하면 출력
+if result:
+    print(result.name, result.age)
