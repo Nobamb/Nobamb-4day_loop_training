68f8e194a6106f00d47668c1fe090bf5f421aeb5
nobamb <jsh12345644@gmail.com>
2026-01-08 20:48:20 +0900

    training2.py 생성 및 테스트

---
+++ training2.py
@@ -0,0 +1,32 @@
+# ### A-02. 생성자로 속성 초기화
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# ```
+
+# **출력**
+
+# ```python
+# print(Person)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# <class '__main__.Person'>
+
+# ```
