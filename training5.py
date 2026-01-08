692b4e34f88f167458496e4d328804d25a01ae1a
nobamb <jsh12345644@gmail.com>
2026-01-08 21:16:08 +0900

    training5.py 생성 및 테스트

---
+++ training5.py
@@ -0,0 +1,41 @@
+# ### A-05. 인스턴스 속성 수정
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# person = Person("공욱재", 26)
+# person.name = "공미남"
+# person.age = 27
+
+# ```
+
+# **출력**
+
+# ```python
+# print(person.name)
+# print(person.age)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 공미남
+# 27
+
+# ```
+
+
+
