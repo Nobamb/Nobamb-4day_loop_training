8db2f6bc7704c338bd25270346adc103ec9dd3d5
nobamb <jsh12345644@gmail.com>
2026-01-08 21:11:12 +0900

    training3 완료

--- training3.py
+++ training3.py
@@ -32,3 +32,20 @@
 # <__main__.Person object at 0x...>
 
 # ```
+
+
+
+# Person 클래스 받음
+# 생성자 name, age
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+# Person 지정(new x)
+# name = 공욱재
+# age = 26
+person = Person("공욱재",26)
+
+# 출력
+print(person)
