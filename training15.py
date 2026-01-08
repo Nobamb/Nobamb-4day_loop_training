dbaeec369735d547dfe626463866945fd9ffe75a
nobamb <jsh12345644@gmail.com>
2026-01-08 23:23:46 +0900

    training15.py 생성 및 테스트

---
+++ training15.py
@@ -0,0 +1,37 @@
+# ### B-08. 문자열 표현 메서드 (**str**)
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def __str__(self):
+#         return self.name + " " + str(self.age)
+
+# person = Person("공욱재", 26)
+
+# ```
+
+# **출력**
+
+# ```python
+# print(person)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 공욱재 26
+
+# ```
