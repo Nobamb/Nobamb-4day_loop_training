784992af4102cc6732535c47883406b7663e3cf2
nobamb <jsh12345644@gmail.com>
2026-01-08 23:18:16 +0900

    training13 완료

--- training13.py
+++ training13.py
@@ -37,3 +37,25 @@
 # 공욱재 26
 
 # ```
+
+
+
+# Person 클래스 생성
+# name, age 생성자 받음
+# display 메서드
+# name, age 출력
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    # 메서드 display
+    def display(self):
+        print(self.name, self.age)
+        
+        
+# person 생성
+person = Person("공욱재",26)
+# 메소드 실행
+person.display()
