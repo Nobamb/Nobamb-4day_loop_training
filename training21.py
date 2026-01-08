10f45631533d60f139f309bbdea30851118c3a35
nobamb <jsh12345644@gmail.com>
2026-01-09 00:05:07 +0900

    training21 완료

--- training21.py
+++ training21.py
@@ -45,3 +45,49 @@
 # 공미남 27
 
 # ```
+
+# index값 받음
+idx = int(input())
+
+
+
+# Person 클래스 생성
+# name, age 생성자 받음
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+# PersonManage 클래스 생성
+class PersonManage:
+    # manage빈배열 생성자
+    def __init__(self):
+        self.manage = []
+        
+    # 배열 추가
+    # name, age를 통해
+    # person 생성
+    def add(self, name, age):
+        person = Person(name, age)
+        # 배열에 person추가
+        self.manage.append(person)
+        
+    # 인덱스 받아서 출력
+    def read_by_index(self, idx):
+        # idx에 해당하는 배열값 반환
+        return self.manage[idx]
+    
+    
+# PersonManage 생성자 생성
+manager = PersonManage()
+# 추가 
+
+manager.add("공욱재", 26)
+manager.add("공미남", 27)
+
+
+# 실행
+person = manager.read_by_index(idx)
+# 출력
+print(person.name, person.age)
