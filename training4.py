41926c2c25af23b6f1d02dc7c4624f9832c73994
nobamb <jsh12345644@gmail.com>
2026-01-08 21:15:14 +0900

    training4 완료

--- training4.py
+++ training4.py
@@ -34,3 +34,22 @@
 # 26
 
 # ```
+
+
+
+# class 선언
+# Person 클래스, name, age 받음
+class Person:
+    def __init__(self,name, age):
+        self.name = name
+        self.age = age
+        
+# person 받음
+# 공욱재, 26
+person = Person("공욱재",26)
+
+# 하나씩 출력
+# 이름
+print(person.name)
+# 나이
+print(person.age)
