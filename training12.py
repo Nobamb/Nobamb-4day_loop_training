1d7a37740c8d8c7f032a2817f32e9103c360fee4
nobamb <jsh12345644@gmail.com>
2026-01-08 23:14:30 +0900

    training12 완료

--- training12.py
+++ training12.py
@@ -36,3 +36,27 @@
 # 30
 
 # ```
+
+
+
+
+# Person 클래스 생성
+# name, age 받음
+# set_age를 통해 값 변경
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def set_age(self, new_age):
+        # age의 값을 new_age로 변경
+        self.age = new_age
+        
+# person 적용
+person = Person("공욱재",26)
+
+# person의 나이 변경
+person.set_age(31)
+
+# 출력
+print(person.age)
