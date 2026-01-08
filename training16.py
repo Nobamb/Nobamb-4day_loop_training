530db956e129a994d579bc8b5fe73c6bc9ba25c0
nobamb <jsh12345644@gmail.com>
2026-01-08 23:28:23 +0900

    training16.py 생성 및 테스트

---
+++ training16.py
@@ -0,0 +1,40 @@
+# ## 섹션 C. 데이터 클래스와 관리 클래스 분리
+
+# ### C-01. 데이터 클래스 정의 (Person)
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def __str__(self):
+#         return self.name + " " + str(self.age)
+
+# ```
+
+# **출력**
+
+# ```python
+# person = Person("공욱재", 26)
+# print(person)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 공욱재 26
+
+# ```
+
+
