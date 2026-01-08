bde827c71bad69e46f90940e88c3894d0ea0a1d8
nobamb <jsh12345644@gmail.com>
2026-01-08 23:15:16 +0900

    training13.py 생성 및 완료

---
+++ training13.py
@@ -0,0 +1,39 @@
+# ---
+
+# ### B-06. 정보 출력 메서드
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def display(self):
+#         print(self.name, self.age)
+
+# person = Person("공욱재", 26)
+
+# ```
+
+# **출력**
+
+# ```python
+# person.display()
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 공욱재 26
+
+# ```
