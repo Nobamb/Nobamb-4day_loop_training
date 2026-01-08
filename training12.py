48b0b6cc3a08f946f12ec61800b0a2ef82a37c0e
nobamb <jsh12345644@gmail.com>
2026-01-08 23:11:32 +0900

    training12.py 생성 및 테스트

---
+++ training12.py
@@ -0,0 +1,38 @@
+# ### B-05. 속성 수정 메서드
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def set_age(self, new_age):
+#         self.age = new_age
+
+# person = Person("공욱재", 26)
+# person.set_age(30)
+
+# ```
+
+# **출력**
+
+# ```python
+# print(person.age)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 30
+
+# ```
