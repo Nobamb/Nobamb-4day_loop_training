98088aaf1d6b74388616f8cde76b23ea05ae7990
nobamb <jsh12345644@gmail.com>
2026-01-08 21:12:13 +0900

    training4.py 생성 및 테스트

---
+++ training4.py
@@ -0,0 +1,36 @@
+# ### A-04. 인스턴스 속성 접근
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# person = Person("공욱재", 26)
+
+# ```
+
+# **출력**
+
+# ```python
+# print(person.name)
+# print(person.age)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 공욱재
+# 26
+
+# ```
