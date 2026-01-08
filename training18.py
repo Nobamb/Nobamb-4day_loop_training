d851fb3d376b3200ac40e9089b9d50f1c886872a
nobamb <jsh12345644@gmail.com>
2026-01-08 23:42:46 +0900

    training18.py 생성 및 테스트

---
+++ training18.py
@@ -0,0 +1,37 @@
+# ### C-03. 관리 클래스에 리스트 속성
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# class PersonManager:
+#     def __init__(self):
+#         self.members = []
+
+# ```
+
+# **출력**
+
+# ```python
+# manager = PersonManager()
+# print(manager.members)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# []
+
+# ```
