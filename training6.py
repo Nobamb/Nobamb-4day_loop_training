ad0f8f1ade0e466ed4722af52c24d85411dfdf63
nobamb <jsh12345644@gmail.com>
2026-01-08 21:19:23 +0900

    training6.py 생성 및 테스트

---
+++ training6.py
@@ -0,0 +1,40 @@
+# ### A-06. 여러 인스턴스 생성
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# person1 = Person("공욱재", 26)
+# person2 = Person("공미남", 27)
+# person3 = Person("공추남", 28)
+
+# ```
+
+# **출력**
+
+# ```python
+# print(person1.name)
+# print(person2.name)
+# print(person3.name)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 공욱재
+# 공미남
+# 공추남
+
+# ```
