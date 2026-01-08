a8a2ced6778099ba8afbb3a13d263c433a64e2ba
nobamb <jsh12345644@gmail.com>
2026-01-08 21:26:32 +0900

    training8.py 생성 및 테스트

---
+++ training8.py
@@ -0,0 +1,37 @@
+# ### B-01. 매개변수 없는 메서드
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def greet(self):
+#         print("안녕하세요")
+
+# person = Person("공욱재", 26)
+
+# ```
+
+# **출력**
+
+# ```python
+# person.greet()
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 안녕하세요
+
+# ```
