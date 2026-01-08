7191b22a05c871ba5d39264669fc43ee4e418dad
nobamb <jsh12345644@gmail.com>
2026-01-08 21:09:03 +0900

    training3.py 생성 및 테스트

---
+++ training3.py
@@ -0,0 +1,34 @@
+# ### A-03. 인스턴스 생성
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# person = Person("공욱재", 26)
+
+# ```
+
+# **출력**
+
+# ```python
+# print(person)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# <__main__.Person object at 0x...>
+
+# ```
