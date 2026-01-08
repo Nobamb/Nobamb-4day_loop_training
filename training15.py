6d71e7b3ab842f93961083f7956c25dbc557156a
nobamb <jsh12345644@gmail.com>
2026-01-08 23:26:51 +0900

    training15 완료

--- training15.py
+++ training15.py
@@ -35,3 +35,26 @@
 # 공욱재 26
 
 # ```
+
+
+# 클래스 Person 생성
+# name, age 생성자 받음
+# __str__
+# name, age 출력
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    # str
+    # name, age 반환
+    def __str__(self):
+        return self.name + " "+str(self.age)
+    
+    
+    
+# person 생성
+person =  Person("공욱재",26)
+
+# person 출력
+print(person)
