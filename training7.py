7853907870859ee5cd72b6d76c6077e503b9f13c
nobamb <jsh12345644@gmail.com>
2026-01-08 21:22:37 +0900

    training7.py 생성 및 테스트

---
+++ training7.py
@@ -0,0 +1,40 @@
+# ### A-07. 인스턴스를 리스트에 저장
+
+# **입력**
+
+# ```
+# 없음
+
+# ```
+
+# **처리**
+
+# ```python
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# members = []
+# members.append(Person("공욱재", 26))
+# members.append(Person("공미남", 27))
+# members.append(Person("공추남", 28))
+
+# ```
+
+# **출력**
+
+# ```python
+# for member in members:
+#     print(member.name, member.age)
+
+# ```
+
+# **실행 결과**
+
+# ```
+# 공욱재 26
+# 공미남 27
+# 공추남 28
+
+# ```
