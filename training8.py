dea33c438dd11831026bbe99b298f7798f645081
nobamb <jsh12345644@gmail.com>
2026-01-08 21:29:44 +0900

    training8완료

--- training8.py
+++ training8.py
@@ -35,3 +35,25 @@
 # 안녕하세요
 
 # ```
+
+
+# Person 클래스 생성
+# name, age받음
+# 매개변수 없는 greet 메서드 생성
+# greet는 "안녕하세요"출력
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def greet(self):
+        print("안녕하세요")
+        
+        
+        
+# Person 생성자 받음
+person = Person("공욱재",26)
+
+# 메서드 테스트
+person.greet()
