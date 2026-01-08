b3281d77d809249827f1aedeaf112eb6032226c4
nobamb <jsh12345644@gmail.com>
2026-01-08 21:21:40 +0900

    training6 완료

--- training6.py
+++ training6.py
@@ -38,3 +38,23 @@
 # 공추남
 
 # ```
+
+
+# Person 클래스 생성
+# name, age 받음
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+        
+# 공욱재 공미남 공추남 받음
+person1 = Person("공욱재",26)
+person2 = Person("공미남",27)
+person3 = Person("공추남",28)
+
+# 출력
+# 이름만
+print(person1.name)
+print(person2.name)
+print(person3.name)
